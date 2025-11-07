7aa7aa17a23057ce9768f0d7841704b97d91b834
berylrafi26 <berylrafi3@gmail.com>
2025-11-07 12:37:33 +0700

    buat screen 3

---
+++ lib/screen/splash_screen_3.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_uts/screen/login.dart';
+
+class MySplashScreen3 extends StatelessWidget {
+  const MySplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          //TODO
+          //1.BUAT IMAGE dalam container
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage("assets/images/jawir_1.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Welcome3",
+              style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 2),
+            Text(
+              "Kasih Paham Tipis Tipis Lah\n Biar Apa Biar Kane Dikit",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14.0, color: Colors.orange),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    color: Colors.orange[100]!,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    color: Colors.orange[100]!,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 40, right: 40),
+              child: SizedBox(
+                height: 25,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                      (Route<dynamic> route) => false,
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                  ),
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
+          //2.BUAT TITLE Welcome
+          //3. buat subtitle lebih kecil warna berbeda
+          //4. buat bulatann kecil ada 3 melebar ( row)
+          //5. buat butto continue dengan warna putih
+        ),
+      ),
+    );
+  }
+}
