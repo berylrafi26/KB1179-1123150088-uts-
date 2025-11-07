699c80319bfb6e08fd2c630db26f583f8c52f461
berylrafi26 <berylrafi3@gmail.com>
2025-11-07 12:54:09 +0700

    mengubah warna di page login dan juga screen dari 1 sampai 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kb1179_1123150088_uts/screen/splash_screen_1.dart';
-import 'package:latihan_uts/screen/splash_screen_1.dart';
 
 void main() {
   runApp(const MyApp());
